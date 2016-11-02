c7e5492b75b2c62b69579a585c214bca2ae5d4f4
Barry <barrylockwood.swdev@gmail.com>
2016-11-02 00:48:10 -0700

    cleaning up key handling

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ subscriptions = {} #doesn't really matter what the value is
 
 def add_contact(phone_number, name, age):
     phone_number = str(phone_number)
-    try:
-        contacts[phone_number] #check existence
-    except KeyError:
+    if phone_number not in contacts:
         contacts[phone_number] = {
             "name": name,
             "age": age
@@ -17,32 +15,25 @@ def update_contact(phone_number, key, value):
     phone_number = str(phone_number)
     key = str(key).lower()
 
-    try:
-        contacts[phone_number]
-    except KeyError:
+    if phone_number not in contacts:
         print("Contact does not exist for phone number: {}".format(phone_number))
     else:
-        try:
-            contacts[phone_number][key]
+        if key in contacts[phone_number]:
             contacts[phone_number][key] = value
-            try:
-                subscriptions[phone_number]
+
+            if phone_number in subscriptions:
                 print("You are subscribed to {} and it changed. Name: {} Age: {}".format(
-                      phone_number, contacts[phone_number]["name"], contacts[phone_number]["age"]))
-            except KeyError:
-                pass
-        except KeyError:
+                    phone_number, contacts[phone_number]["name"], contacts[phone_number]["age"]))
+        else:
             print("Key {} does not exist for contact with phone number {}".format(key, phone_number))
 
 
-
-
 def find_contact(phone_number):
-    try:
-        contact = contacts[str(phone_number)]
+    phone_number = str(phone_number)
+
+    if phone_number in contacts:
+        contact = contacts[phone_number]
         print("Contact Name: {} Age: {}".format(contact["name"], contact["age"]))
-    except KeyError:
-        print("Could not find contact with phone number: {}".format(phone_number))
 
 def subscribe(phone_number):
     subscriptions[str(phone_number)] = True
